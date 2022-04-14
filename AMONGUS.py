8d9cb13125532bad67287cc1378d3725187b565b
Logikitty <96258098+Logikitty@users.noreply.github.com>
2022-04-13 21:48:34 -0500

    Create AMONGUS.py

---
+++ AMONGUS.py
@@ -0,0 +1,7 @@
+print ("hi")
+among us = amongIs?
+amonus = BRUH
+sUSy = WaiT YoU dOnT SpeLL LikE DaT?!? ez sussy lol
+print ("AMONG US AMONUS IS AMONG US SUSSY BAKA AMONG US DABEBE KANGARUO? AMONG US SUSSSSSSSSSSY?? NO YES? NO.... CUZ AMONG US SUSSY BAKA AMONG US NOPEEEE BEBE!?!?? NOPEEEEEEEEEEEEEEEE BRO PEEEEEEEEEEEE AMONG US SUSSY BAKA NA COW COW COW COW MOO MOOO MOOOMOOO.IO BAKA SUSSSY BAKA I READ WINGS OF FIRE YEEEEEEEEEEEE COW MOO MOO MOOO I LISSEN TO THE BOOK ON HOOPLA OOOY?")
+ankjdnxna = iundiwxsanaxakmxonaexnosxnwjsndcwegcuygsuybssisjnxijwndijcnsdnicunduicnduncudsnuncn
+#bruhhhhhhhhhunfqnifncnnvijanrfubrbykbdbfidjadbiva jd c djchc aicncj c adfbvjhtrj(btyugbcbfwnojnf nbugvfg g  b vina srihfbvknc  sbbvugsdvyuun gbiwefbfbnghvuygb aifjbbguyvhk(vriubfuhwabiuaihbvjhvbrnwbjnkw& among us sssssuuuuuussssssssssyyyyyy bakaaaaassaass aaaaakaaaaaaa OwO cow mooooooooooooooooooooooo cow mooooooooooooo amongussussybaka
